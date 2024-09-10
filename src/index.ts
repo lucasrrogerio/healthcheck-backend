ba5313e4e5bda5a4a62dd13ea67356fb308f88e7
Lucas <rogerio.ferreira.ext@ipiranga.ipiranga>
2024-09-10 11:17:52 -0300

    adição da verificação 10minutos

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import { router as logRouter } from "./route/applicationStatus"
 import { router as healthRouter } from "./route/health"
+import { applicationStatusService } from "./service/applicationStatus";
 
 const app: Express = express();
 
@@ -11,5 +12,11 @@ const apiVersion = "/api/v1"
 app.use(cors());
 app.use(`${apiVersion}/logs`, logRouter);
 app.use(`${apiVersion}/health`, healthRouter)
+
+function minutosParaMillis(minutos: number) {
+    return minutos * 60000;
+}
+
+setInterval(applicationStatusService.getAllAppStatusRecent, minutosParaMillis(10));
 
 export { app };
